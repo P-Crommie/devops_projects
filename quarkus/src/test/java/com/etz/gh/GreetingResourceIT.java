f1bdd8c58fda0ec5301b6f4ebe090da0dd3866f7
crommie <immanuelcromwell@ymail.com>
2023-08-21 14:33:03 +0000

    FIX: hotfix to dirs

--- quarkus/src/test/java/com/etz/gh/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.etz.gh;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
